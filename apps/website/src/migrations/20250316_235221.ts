ab7b8157be38f5e5ba2b68b7088e390cb61f14f8
Silas Krause <krausesilas@gmail.com>
2025-03-17 00:57:34 +0100

    Add migration for appendix

---
+++ apps/website/src/migrations/20250316_235221.ts
@@ -0,0 +1,13 @@
+import { type MigrateUpArgs, type MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+	await db.execute(sql`
+   ALTER TABLE "posts" ADD COLUMN "appendix" jsonb DEFAULT '{"root":{"type":"root","children":[{"tag":"h2","type":"heading","children":[{"text":"Quellen","type":"text"}]}]}}';
+  ALTER TABLE "_posts_v" ADD COLUMN "version_appendix" jsonb DEFAULT '{"root":{"type":"root","children":[{"tag":"h2","type":"heading","children":[{"text":"Quellen","type":"text"}]}]}}';`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+	await db.execute(sql`
+   ALTER TABLE "posts" DROP COLUMN IF EXISTS "appendix";
+  ALTER TABLE "_posts_v" DROP COLUMN IF EXISTS "version_appendix";`)
+}
--- apps/website/src/migrations/index.ts
+++ apps/website/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250305_160427 from './20250305_160427';
+import * as migration_20250316_235221 from './20250316_235221';
 
 export const migrations = [
   {
     up: migration_20250305_160427.up,
     down: migration_20250305_160427.down,
-    name: '20250305_160427'
+    name: '20250305_160427',
+  },
+  {
+    up: migration_20250316_235221.up,
+    down: migration_20250316_235221.down,
+    name: '20250316_235221'
   },
 ];
