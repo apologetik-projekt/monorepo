97febc7ed3e9f3fd82cf8aa5211d90ab1eed57bb
Silas Krause <krausesilas@gmail.com>
2025-03-26 03:03:14 +0100

    refactor(instrumentation): make register function async and lazy load dependencies
    Convert the register function to be asynchronous and use dynamic imports for `@azure/functions-core` and `applicationinsights`. This change improves the initialization process by deferring the loading of dependencies until they are needed, reducing the initial load time and improving performance in environments where these dependencies are not immediately required.

--- apps/website/src/instrumentation.ts
+++ apps/website/src/instrumentation.ts
@@ -1,17 +1,22 @@
-import appInsights from 'applicationinsights'
-export function register() {
-	appInsights
-		.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
-		.setAutoCollectConsole(true)
-		.setAutoCollectDependencies(true)
-		.setAutoCollectExceptions(true)
-		.setAutoCollectHeartbeat(true)
-		.setAutoCollectPerformance(true, true)
-		.setAutoCollectRequests(true)
-		.setAutoDependencyCorrelation(true)
-		.setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-		.setSendLiveMetrics(true)
-		.setUseDiskRetryCaching(true)
-	appInsights.defaultClient.setAutoPopulateAzureProperties()
-	appInsights.start()
+export async function register() {
+	// @ts-expect-error
+	import('@azure/functions-core')
+		.then(async () => {
+			const appInsights = await import('applicationinsights')
+			appInsights
+				.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+				.setAutoCollectConsole(true)
+				.setAutoCollectDependencies(true)
+				.setAutoCollectExceptions(true)
+				.setAutoCollectHeartbeat(true)
+				.setAutoCollectPerformance(true, true)
+				.setAutoCollectRequests(true)
+				.setAutoDependencyCorrelation(true)
+				.setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+				.setSendLiveMetrics(true)
+				.setUseDiskRetryCaching(true)
+			appInsights.defaultClient.setAutoPopulateAzureProperties()
+			appInsights.start()
+		})
+		.catch(() => {})
 }
