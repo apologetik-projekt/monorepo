54439ad79944373ac8c4186f8004ccb576bc6b34
Silas Krause <krausesilas@gmail.com>
2025-03-26 21:44:03 +0100

    ci: add NEXT_PUBLIC_SERVER_URL env variable to Azure workflow
    refactor: remove azureApplicationInsights plugin and update instrumentation
    
    The azureApplicationInsights plugin was removed as it was redundant with the instrumentation.ts file. The instrumentation.ts file was updated to conditionally initialize Application Insights only if the connection string is present, improving robustness and error handling.

--- apps/website/src/instrumentation.ts
+++ apps/website/src/instrumentation.ts
@@ -1,22 +1,23 @@
 export async function register() {
 	try {
-		require.resolve('@azure/functions-core')
-		const appInsights = await import('applicationinsights')
-		appInsights
-			.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
-			.setAutoCollectConsole(true)
-			.setAutoCollectDependencies(true)
-			.setAutoCollectExceptions(true)
-			.setAutoCollectHeartbeat(true)
-			.setAutoCollectPerformance(true, true)
-			.setAutoCollectRequests(true)
-			.setAutoDependencyCorrelation(true)
-			.setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-			.setSendLiveMetrics(true)
-			.setUseDiskRetryCaching(true)
-		appInsights.defaultClient.setAutoPopulateAzureProperties()
-		appInsights.start()
+		if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+			const appInsights = await import('applicationinsights')
+			appInsights
+				.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+				.setAutoCollectConsole(true)
+				.setAutoCollectDependencies(true)
+				.setAutoCollectExceptions(true)
+				.setAutoCollectHeartbeat(true)
+				.setAutoCollectPerformance(true, true)
+				.setAutoCollectRequests(true)
+				.setAutoDependencyCorrelation(true)
+				.setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+				.setSendLiveMetrics(true)
+				.setUseDiskRetryCaching(true)
+			appInsights.defaultClient.setAutoPopulateAzureProperties()
+			appInsights.start()
+		}
 	} catch (e) {
-		//
+		console.error('You tried to run application insight in an environment thats not supported')
 	}
 }
