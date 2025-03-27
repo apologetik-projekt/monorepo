30cd54def5d928d28e68bb04fd4877a06a4a1693
Silas Krause <krausesilas@gmail.com>
2025-03-27 18:50:48 +0100

    Add migration

--- apps/website/src/migrations/index.ts
+++ apps/website/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250305_160427 from './20250305_160427';
 import * as migration_20250316_235221 from './20250316_235221';
+import * as migration_20250327_174959 from './20250327_174959';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250316_235221.up,
     down: migration_20250316_235221.down,
-    name: '20250316_235221'
+    name: '20250316_235221',
+  },
+  {
+    up: migration_20250327_174959.up,
+    down: migration_20250327_174959.down,
+    name: '20250327_174959'
   },
 ];
