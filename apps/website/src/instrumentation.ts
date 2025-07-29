e8e0f059ce89871451b6bf5a35b3269772e34aa3
Silas Krause <krausesilas@gmail.com>
2025-07-29 22:41:01 +0200

    Simplify sentry init

--- apps/website/src/instrumentation.ts
+++ apps/website/src/instrumentation.ts
@@ -1,12 +1,19 @@
 import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
-	if (process.env.NEXT_RUNTIME === 'nodejs') {
-		await import('../sentry.server.config')
-	}
+	if (process.env.NODE_ENV === 'production') {
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+		Sentry.init({
+			dsn: 'https://de5b08ff5e46343a42a477c195d00e50@o4509378229633024.ingest.de.sentry.io/4509378238546000',
 
-	if (process.env.NEXT_RUNTIME === 'edge') {
-		await import('../sentry.edge.config')
+			// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+			tracesSampleRate: 1,
+
+			// Setting this option to true will print useful information to the console while you're setting up Sentry.
+			debug: false,
+
+			enabled: !!process.env.SENTRY_AUTH_TOKEN,
+		})
 	}
 }
 
