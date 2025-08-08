ee28993700aedd09d7b2e64d2295fed04c614399
Silas Krause <krausesilas@gmail.com>
2025-08-08 22:08:04 +0200

    Move media folder

--- packages/payload/collections/Media.ts
+++ packages/payload/collections/Media.ts
@@ -27,7 +27,7 @@ export const Media: CollectionConfig = {
 	folders: true,
 	upload: {
 		// Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-		staticDir: path.resolve(dirname, '../../../apps/website/public/media'),
+		staticDir: path.resolve(dirname, '../media'),
 		adminThumbnail: 'thumbnail',
 		focalPoint: true,
 		imageSizes: [
