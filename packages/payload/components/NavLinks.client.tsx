71b3b3a61f9493f331b42fa340121d61409a960d
Silas Krause <krausesilas@gmail.com>
2025-08-11 02:44:19 +0200

    Cleanup

--- packages/payload/components/NavLinks.client.tsx
+++ packages/payload/components/NavLinks.client.tsx
@@ -5,20 +5,19 @@ import { usePathname } from 'next/navigation'
 import type React from 'react'
 
 export function NavLink({
-  href,
-  id,
-  children,
+	href,
+	id,
+	children,
 }: React.PropsWithChildren<{ href: string; id: string }>) {
-  const pathname = usePathname()
-  return (
-    <Link
-      //style={{ fontSize: 13.33 }}
-      className={'nav__link' + (pathname === href ? ' active' : '')}
-      id={'#nav-' + id}
-      href={href}
-    >
-      {pathname === href ? <div className="nav__link-indicator"></div> : null}
-      <span className="nav__link-label">{children}</span>
-    </Link>
-  )
+	const pathname = usePathname()
+	return (
+		<Link
+			className={'nav__link' + (pathname === href ? ' active' : '')}
+			id={'#nav-' + id}
+			href={href}
+		>
+			{pathname === href ? <div className="nav__link-indicator"></div> : null}
+			<span className="nav__link-label">{children}</span>
+		</Link>
+	)
 }
